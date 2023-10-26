97bb9067743df486be23a41ae8aac1cc2a06925c
Jonathan <jonathanking.dev@gmail.com>
2023-10-26 17:16:11 +0200

    commit solution

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,63 @@
 import "./style.css";
 
-const popRock = "🎵 Pop rock";
-const rock = "🎸 Rock";
-const hardRock = "🤘 HardRock";
-const clasica = "🎼 Clasica";
+let turnoActual: number = 1;
 
-const estilo = "background-color: green; font-size: 30px; font-weight: bold;";
-
-interface grupoMusical {
-  nombreGrupo: string;
-  fundacion: number;
-  enActivo: boolean;
-  genero: string;
+function getTurno(): string {
+  if (turnoActual < 10) {
+    return "0" + turnoActual;
+  }
+  return turnoActual.toString();
 }
 
-const grupo1: grupoMusical = {
-  nombreGrupo: "The Beatles",
-  fundacion: 1960,
-  enActivo: true,
-  genero: popRock,
-};
-const grupo2: grupoMusical = {
-  nombreGrupo: "Queen",
-  fundacion: 1970,
-  enActivo: false,
-  genero: rock,
-};
-const grupo3: grupoMusical = {
-  nombreGrupo: "AC DC",
-  fundacion: 1973,
-  enActivo: true,
-  genero: hardRock,
-};
-const grupo4: grupoMusical = {
-  nombreGrupo: "Ludwig van Beethoven",
-  fundacion: 1770,
-  enActivo: false,
-  genero: clasica,
-};
-const grupo5: grupoMusical = {
-  nombreGrupo: "The Rolling Stones",
-  fundacion: 1962,
-  enActivo: true,
-  genero: rock,
-};
-
-function print(g: grupoMusical) {
-  return g.fundacion + " / Activo: " + g.enActivo + " / " + g.genero;
+function printTurno(): void {
+  let resultadoElement = document.getElementById("numeroTurno");
+  if (resultadoElement !== null && resultadoElement !== undefined) {
+    resultadoElement.innerHTML = getTurno();
+  }
 }
 
-console.log("%c" + grupo1.nombreGrupo, estilo, print(grupo1));
-console.log("%c" + grupo2.nombreGrupo, estilo, print(grupo2));
-console.log("%c" + grupo3.nombreGrupo, estilo, print(grupo3));
-console.log("%c" + grupo4.nombreGrupo, estilo, print(grupo4));
-console.log("%c" + grupo5.nombreGrupo, estilo, print(grupo5));
+function sumarTurno(): void {
+  turnoActual = turnoActual + 1;
+  printTurno();
+}
+
+function resetTurno(): void {
+  turnoActual = 1;
+  printTurno();
+}
+
+function restarTurno(): void {
+  turnoActual = turnoActual - 1;
+  printTurno();
+}
+
+function numeroOperario(): void {
+  let numeroManual = parseInt(
+    (document.getElementById("numeroManual") as HTMLInputElement).value
+  );
+  turnoActual = numeroManual;
+  printTurno();
+}
+
+const botonAnterior = document.getElementById("anterior");
+const botonReset = document.getElementById("reset");
+const botonSiguiente = document.getElementById("siguiente");
+const botonCambioTurno = document.getElementById("cambiarTurno");
+
+if (botonSiguiente !== null && botonSiguiente !== undefined) {
+  botonSiguiente.addEventListener("click", sumarTurno);
+}
+
+if (botonAnterior !== null && botonAnterior !== undefined) {
+  botonAnterior.addEventListener("click", restarTurno);
+}
+
+if (botonReset !== null && botonReset !== undefined) {
+  botonReset.addEventListener("click", resetTurno);
+}
+
+if (botonCambioTurno !== null && botonCambioTurno !== undefined) {
+  botonCambioTurno.addEventListener("click", numeroOperario);
+}
+
+printTurno();
